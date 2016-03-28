fe6c63e3c701d049a5a73356fa43d5abeaea698f
tttaaaa <bamboo_river@hotmail.co.jp>
2016-03-28 22:45:20 +0900

    library update

--- app/src/main/java/com/example/yuri/tokaidoapp/MainActivity.java
+++ app/src/main/java/com/example/yuri/tokaidoapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.yuri.tokaidoapp;
 
+import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +19,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
      public void getshindan(View view){
-            Intent intent = new Intent(this,Hantei_mae.class);
-            startActivity(intent);
-            }
+         LocationManager mLocationManager = (LocationManager) this.getSystemService(Service.LOCATION_SERVICE);
+
+
+
+         Intent intent = new Intent(this,Hantei_mae.class);
+         startActivity(intent);
+     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
 
