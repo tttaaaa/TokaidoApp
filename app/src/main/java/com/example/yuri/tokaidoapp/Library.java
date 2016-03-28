7b290ccf9d20acb5aa918daa196a1f5022105591
tttaaaa <bamboo_river@hotmail.co.jp>
2016-03-28 23:30:55 +0900

    Location data

--- app/src/main/java/com/example/yuri/tokaidoapp/Library.java
+++ app/src/main/java/com/example/yuri/tokaidoapp/Library.java
@@ -7,44 +7,7 @@ import android.location.LocationManager;
  */
 public class Library {
 
-    public static String[3] GPS(LocationManager mLocationManager,LocationListener mLOcationListner){
-        DOUBLE [] GPS ={0.0,0.0};
-        boolean isNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        if (isNetworkEnabled) {
-            mLocationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    LOCATION_UPDATE_MIN_TIME,
-                    LOCATION_UPDATE_MIN_DISTANCE,
-                    ｍLocationListner
-            );
-            Geocoder   mGeocoder = new Geocoder(getApplicationContext(), Locale.JAPAN);
-            Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (location != null) {
-                double longitude = location.getLongitude();
-                double latitude = location.getLatitude();
-                GPS[0] =longitude.toString;
-                GPS[1] =latitude.toString;
-                //GeoCoder を用いて地名を検索
-                StringBuffer buff = new StringBuffer();
-                try {
-                    List<Address> addrs = mGeocoder.getFromLocation(latitude, longtitude, 1);
-                    for(Address addr : addrs){
-                        //地名を取得して，文字列に連結する
-                        int index = addr.getMaxAddressLineIndex();
-                        for(int i = 0; i <= index; i++){
-                            buff.append(addr.getAddressLine(i));
-                            buff.append("\n");
-                        }
-                        buff.append("\n");
-                        String city = buff.toString;
-                    }
-                } catch(IOException e){
-                    Log.e("HelloLocationActivity", e.toString());
-                }
-                ｝
-                GPS[2] = city;
-                return GPS ;
-            }
+
 
 
         }
--- app/src/main/java/com/example/yuri/tokaidoapp/MainActivity.java
+++ app/src/main/java/com/example/yuri/tokaidoapp/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.yuri.tokaidoapp;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements LocationListener{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    // 更新時間(目安)
+    private static final int LOCATION_UPDATE_MIN_TIME = 0;
+    // 更新距離(目安)
+    private static final int LOCATION_UPDATE_MIN_DISTANCE = 1;
+
+    private LocationManager mLocationManager;
+
+    public Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +36,63 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
     }
 
      public void getshindan(View view){
-         LocationManager mLocationManager = (LocationManager) this.getSystemService(Service.LOCATION_SERVICE);
+         mLocationManager = (LocationManager) this.getSystemService(Service.LOCATION_SERVICE);
+
+         requestLocationUpdates();
 
 
+
+
+
+         Geocoder mGeocoder = new Geocoder(getApplicationContext(), Locale.JAPAN);
+
+         double longitude = location.getLongitude();
+         double latitude = location.getLatitude();
+         StringBuffer buff = new StringBuffer();
+         try {
+             List<Address> addrs = mGeocoder.getFromLocation(latitude, longitude, 1);
+             for(Address addr : addrs){
+                 //地名を取得して，文字列に連結する
+                 int index = addr.getMaxAddressLineIndex();
+                 for(int i = 0; i <= index; i++){
+                     buff.append(addr.getAddressLine(i));
+                     buff.append("\n");
+                 }
+                 buff.append("\n");
+                 String city = buff.toString();
+                 Log.e("GGGGGGGGGGGGGetCity", city);
+             }
+         } catch(IOException e){
+             Log.e("HelloLocationActivity", e.toString());
+         }
 
          Intent intent = new Intent(this,Hantei_mae.class);
          startActivity(intent);
      }
+
+
+    public void requestLocationUpdates(){
+        boolean isNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (isNetworkEnabled) {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    LOCATION_UPDATE_MIN_TIME,
+                    LOCATION_UPDATE_MIN_DISTANCE,
+                    this
+            );
+
+
+        } else {
+            Log.e("errror", "GPS is error" );
+        }
+
+        location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location == null) {
+            Log.e("errror", "Location=null " );
+        }
+    }
+
+
 
     @Override
     public void onLocationChanged(Location location) {
